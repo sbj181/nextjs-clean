6826f557c9d85378f5968a62ddb749bc43859f4e
Scott Johnson <scottj@thegrovery.com>
2024-05-14 14:25:14 -0400

    updating pnpm lock and reinstall

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/styles/**/*.{css}"
+    "./src/styles/**/*.css"
   ],
   theme: {
     extend: {},
