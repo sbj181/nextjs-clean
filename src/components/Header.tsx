4ee756070563723538a00f76f04a3d19eebef9d1
Scott Johnson <scottj@thegrovery.com>
2024-07-09 11:25:15 -0400

    reducing icon size for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
     <>
       <header className="bg-slate-100 dark:bg-slate-600 py-2 px-4 flex justify-between items-center fixed w-full z-50">
         <Link href="/" passHref>
-          <div className="w-[150px] py-2 cursor-pointer">
+          <div className="w-[125px] sm:w-[140px] md:w-[150px] transition ease-in duration-150  py-2 cursor-pointer">
             <CoreLogo />
           </div>
         </Link>
@@ -24,7 +24,7 @@ export default function Header() {
           <div className="md:hidden mr-2 ml-auto">
             <SearchButton />
           </div>
-          <div className="p-2 bg-slate-50 dark:bg-slate-700 rounded-lg">
+          <div className="p-1 sm:p-2 bg-slate-50 dark:bg-slate-700 rounded-lg">
             <ThemeToggle />
           </div>
           
@@ -32,13 +32,13 @@ export default function Header() {
             
             className="p-2 ml-2 bg-slate-50 dark:bg-slate-700 rounded-lg flex items-center"
           >
-            <span className="inline-block"><FiUser className="h-6 w-6" /></span>
+            <span className="inline-block"><FiUser className="xs:h-5 xs:h-5 sm:h-6 sm:w-6" /></span>
           </Link>
           <button
             onClick={toggleSidebar}
             className="p-2 ml-2 bg-slate-50 dark:bg-slate-700 rounded-lg flex items-center"
           >
-            {isSidebarOpen ? <FiX className="h-6 w-6" /> : <FiMenu className="h-6 w-6" />}
+            {isSidebarOpen ? <FiX className="xs:h-5 xs:h-5 sm:h-6 sm:w-6" /> : <FiMenu className="xs:h-5 xs:h-5 sm:h-6 sm:w-6" />}
             <span className="hidden md:inline-block ml-2">Menu</span>
           </button>
         </div>
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -8,7 +8,7 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ percentage }) => {
   return (
     <div className="w-full bg-gray-200 rounded-full h-4 dark:bg-gray-700">
       <div
-        className="bg-blue-500 h-4 rounded-full"
+        className="bg-green-500 h-4 rounded-full"
         style={{ width: `${percentage}%` }}
       ></div>
     </div>
