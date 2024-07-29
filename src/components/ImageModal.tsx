c233483d30481506c489bc1d7e10373ea9d90e1e
Scott Johnson <scottj@thegrovery.com>
2024-07-29 12:07:47 -0400

    image modal popup

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -1,5 +1,6 @@
 import Modal from 'react-modal';
 import Image from 'next/image';
+import { FiX } from 'react-icons/fi';
 
 const customStyles = {
   content: {
@@ -8,7 +9,14 @@ const customStyles = {
     right: 'auto',
     bottom: 'auto',
     marginRight: '-50%',
+    backgroundColor: 'transparent',
     transform: 'translate(-50%, -50%)',
+    border: '0',
+    padding:'0',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)', // Custom background color for the overlay
+    zIndex: 9999,
   },
 };
 
@@ -22,11 +30,14 @@ const ImageModal = ({ isOpen, onRequestClose, src, alt }) => {
       style={customStyles}
       contentLabel="Enlarged Image"
     >
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center ">
+        <div className='flex w-full justify-end'>
+            <button onClick={onRequestClose} className="mt-4 mb-4 pl-2 pr-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition flex items-center gap-1 ">
+            <FiX /> Close
+            </button>
+        </div>
         <Image src={src} alt={alt} width={800} height={600} />
-        <button onClick={onRequestClose} className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition">
-          Close
-        </button>
+        
       </div>
     </Modal>
   );
